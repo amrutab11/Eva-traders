37e1199d3c8a60bb2a722f9871cee17ed831d813
amrutab11 <amrutabelgaonkar11998@gmail.com>
2023-10-23 15:20:01 +0530

    Router and home component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: HomeComponent },
+  { path: 'about', component: HomeComponent },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
