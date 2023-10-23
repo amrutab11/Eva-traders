cd53db4644f2c3ea159c8a576b1917ca7fdd38b6
Mayur Salunke <mayursalunke@Mayurs-Mac-mini.local>
2023-10-23 23:16:38 +0530

    social media added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,3 +14,20 @@ export class HomeComponent {
     this.router.navigate(['/about']); // Replace '/products' with the desired route path
   }
 }
+window.onload = function() {
+  let images = document.querySelector('.slider');
+  let firstImage = document.querySelector('.hover-image');
+  let container = document.querySelector('.image-container');
+
+  if (images instanceof HTMLElement && firstImage instanceof HTMLElement && container instanceof HTMLElement) {
+      let imageWidth = (firstImage as HTMLElement).offsetWidth;
+      let totalWidth = images.children.length * imageWidth;
+      let containerWidth = (container as HTMLElement).offsetWidth;
+
+      if (totalWidth > containerWidth) {
+          (images as HTMLElement).style.width = totalWidth + 'px';
+      }
+  }
+};
+
+
